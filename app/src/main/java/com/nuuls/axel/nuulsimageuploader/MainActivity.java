186b8d894560f281e0aed1d84404347b2eb23c70
Felix Schmid <felixschmidbb@gmail.com>
2019-10-08 22:23:31 +0200

    Fixed memory leaks and refactored code

--- app/src/main/java/com/nuuls/axel/nuulsimageuploader/MainActivity.java
+++ app/src/main/java/com/nuuls/axel/nuulsimageuploader/MainActivity.java
@@ -1,6 +1,11 @@
 package com.nuuls.axel.nuulsimageuploader;
 
 import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -10,13 +15,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -29,13 +33,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
-import static android.os.Environment.getExternalStoragePublicDirectory;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HttpPostTask.onUploadedCallback {
     private static final int PERMISSION_REQUEST_CODE = 1;
-    // bitmap has to be public static to set it to null to avoid uploading the same pic pressing the upload button
-    public static Bitmap bitmap;
+    private static final int CAMERA_REQUEST_CODE = 2;
     private TextView textView;
     private ImageView image;
     private Button btnCamera;
@@ -44,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     private File photoFile;
     private Uri photoFileUri;
     private String picPath;
+    private boolean tookPicture = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,8 +64,7 @@ public class MainActivity extends AppCompatActivity {
         String action = intent.getAction();
         String type = intent.getType();
 
-
-        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 == PackageManager.PERMISSION_GRANTED) {
             // if im receiving an image from other app
             if (Intent.ACTION_SEND.equals(action) && type != null) {
@@ -77,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (intent.resolveActivity(getPackageManager()) != null) {
             // asking permission to write in /Pictures
-            if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
                 // if it has no permission, asks for it
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     enablePermission();
@@ -85,66 +87,56 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        btnCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // if the user granted permission to save files
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        == PackageManager.PERMISSION_GRANTED) {
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-                    try {
-                        photoFile = createImageFile();
-                        picPath = photoFile.getAbsolutePath();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    // if the photoFile contains the picture
-                    if (photoFile != null) {
-                        intent = getPhotoFileUri(intent, photoFile);
-                        // callback to the activity after the camera activity
-                        startActivityForResult(intent, 0);
-                    }
-                } else {
-                    forceAcceptPermission();
+        btnCamera.setOnClickListener(v -> {
+            // if the user granted permission to save files
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                try {
+                    photoFile = createImageFile();
+                    picPath = photoFile.getAbsolutePath();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+                // if the photoFile contains the picture
+                if (photoFile != null) {
+                    // callback to the activity after the camera activity
+                    startActivityForResult(getPhotoFileUri(photoFile), CAMERA_REQUEST_CODE);
+                }
+            } else {
+                forceAcceptPermission();
             }
         });
 
-        btnUpload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // if the user granted permission to save files
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        == PackageManager.PERMISSION_GRANTED) {
-                    // check if the pic hasn't been taken yet so the button doesn't try to upload nothing
-                    if (bitmap == null) {
-                        generateToast("Take a picture first :D");
-                        return;
-                    }
-                    // change the color of the button to the wheel that appears when uploading in the website
-                    btnUpload.setBackgroundColor(Color.parseColor("#de2761"));
-                    btnCamera.setEnabled(false);
-                    btnUpload.setEnabled(false);
-                    btnUpload.setText("UPLOADING...");
-                    // TODO: add a NaM face to imageView while image is being uploaded
-                    // start the thread for the upload
-                    HttpPostTask httpPostTask = new HttpPostTask(MainActivity.this, image, btnUpload, btnCamera, textView, bitmap);
-                    httpPostTask.execute(picPath);
-
-                } else {
-                    forceAcceptPermission();
+        btnUpload.setOnClickListener(v -> {
+            // if the user granted permission to save files
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                // check if the pic hasn't been taken yet so the button doesn't try to upload nothing
+                // and crashes the app 4HEad
+                if (!tookPicture) {
+                    generateToast("Take a picture first :D");
+                    return;
                 }
+                // change the color of the button to the wheel that appears when uploading in the website
+                btnUpload.setBackgroundColor(Color.parseColor("#de2761"));
+                btnCamera.setEnabled(false);
+                btnUpload.setEnabled(false);
+                btnUpload.setText(R.string.uploading);
+                //start the thread for the upload
+                HttpPostTask httpPostTask = new HttpPostTask(MainActivity.this);
+                httpPostTask.execute(picPath);
+            } else {
+                forceAcceptPermission();
             }
         });
     }
 
     private void forceAcceptPermission() {
-        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
             // if the user didnt accept the permissions
             enablePermission();
             // if the user checked the "never show again"
-            if (!ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                 enablePermission();
             }
@@ -152,38 +144,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void enablePermission() {
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
     }
 
     private void handleSendImage(Intent intent) {
-
         final Uri imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
-        try {
-            // get the bitmap from the uri
-            Log.e("IMAGE URI>", imageUri.toString());
-            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // after the layout is built and sizes are set
-        // java.lang.IllegalArgumentException: width and height must be > 0  -> I avoid this error using this
-        image.post(new Runnable() {
-            @Override
-            public void run() {
-                Bitmap bitmapResized = Bitmap.createScaledBitmap(bitmap, image.getWidth(), image.getHeight(), true);
-                image.setImageBitmap(bitmapResized);
-            }
-        });
+        image.setImageURI(imageUri);
 
         try {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
             photoFile = createImageFile();
             picPath = photoFile.getAbsolutePath();
-            Log.e("PIC PATH>", picPath);
+
             FileOutputStream fos = new FileOutputStream(photoFile);
             bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
             fos.flush();
             fos.close();
+            tookPicture = true;
             removeExifGeolocation();
         } catch (IOException e) {
             e.printStackTrace();
@@ -191,9 +168,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // gets the URI of the photo and attaches the uri to the picture
-    private Intent getPhotoFileUri(Intent intent, File photoFile) {
+    private Intent getPhotoFileUri(File photoFile) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // returns a content uri for a given file
-        photoFileUri = FileProvider.getUriForFile(getApplicationContext(),
+        photoFileUri = FileProvider.getUriForFile(this,
                 getPackageName() + ".fileprovider",
                 photoFile);
         // attaches the picture to the uri of the file we created before
@@ -205,18 +183,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
+        if (resultCode == RESULT_OK && requestCode == CAMERA_REQUEST_CODE) {
             try {
-                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoFileUri);
-                insertImageIntoImageView();
+                image.setImageURI(photoFileUri);
                 removeExifGeolocation();
+                tookPicture = true;
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else if (resultCode == RESULT_CANCELED) {
             // this deletes the empty file that gets created before taking the picture in case you cancel it
             getContentResolver().delete(photoFileUri, null, null);
-            generateToast("CANCELED");
+            generateToast(getString(R.string.canceled));
         }
     }
 
@@ -242,25 +220,41 @@ public class MainActivity extends AppCompatActivity {
         exif.saveAttributes();
     }
 
-    // Puts the bitmap into the imageView to display the image
-    private void insertImageIntoImageView() {
-        Bitmap bitmapResized = Bitmap.createScaledBitmap(bitmap, image.getWidth(), image.getHeight(), true);
-        // resize the bitmap to display the pic inside the frame
-        image.setImageBitmap(bitmapResized);
+    @Override
+    public void onUpload(String result) {
+        generateToast("Copied: " + result);
+
+        // copy the url to the clipboard
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("URL from nuuls server xd", result);
+        if (clipboard != null) {
+            clipboard.setPrimaryClip(clip);
+        }
+
+        // reset the image to nuulsLogo
+        image.setImageResource(R.drawable.nuulslogo);
+        //display the url
+        textView.setText(result);
+        btnUpload.setBackgroundColor(Color.parseColor("#4caf50"));
+        btnUpload.setText(getString(R.string.upload));
+        btnCamera.setEnabled(true);
+        btnUpload.setEnabled(true);
+        //reset the bitmap so you cant upload the same thing infinitely
+        tookPicture = false;
     }
 
     // creates an image file inside the FileProvider directory
     private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
         String imageFileName = "XD_" + timeStamp + "_";
-        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        File imageFile = File.createTempFile(
+
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        return File.createTempFile(
                 imageFileName,  // prefix
                 ".jpg",   // suffix
                 storageDir      // directory
         );
-
-        return imageFile;
     }
 
     private void generateToast(String text) {
